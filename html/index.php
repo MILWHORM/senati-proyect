4ca897065727b71a03df33666b0f8b0d457366b4
MILWHORM <manjar168@hotmail.com>
2023-10-26 16:15:05 -0500

    se anida la hoja de estilos css

--- html/index.php
+++ html/index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Saludo</title>
 </head>
 <body>
     <h1>Bievenidos</h1>
